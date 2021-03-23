6dfbf29d04318c5d207440cfaed68d32411f5475
Michael Dojčár <michaeldojcar@gmail.com>
2021-03-23 09:47:28 +0100

    added Google Analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,15 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1">
 
-    {{--    <!-- Bootstrap CSS -->--}}
-    {{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"--}}
-    {{--          rel="stylesheet"--}}
-    {{--          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"--}}
-    {{--          crossorigin="anonymous">--}}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EGGCE09GFV"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EGGCE09GFV');
+    </script>
 
     {{--    Fonts--}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -21,6 +25,7 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('css/app.css') }}"
           rel="stylesheet">
